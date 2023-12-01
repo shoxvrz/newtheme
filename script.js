968fe352b56f7e0b2f75ac9c306486a55aa6820b
Shoxruz <shoxruzabdurasulov7@gmail.com>
2023-12-01 14:58:35 +0500

    f

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu2 = document.querySelector("#menu2");
+const menu = document.querySelector("#menu");
+console.log(menu2);
+menu.addEventListener("click", function (){
+    menu2.classList.toggle("hidden");
+
+});
+
